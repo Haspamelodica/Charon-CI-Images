64f8990fa91550572c1a9a5a7e42bd05a5abc3d3
Haspamelodica <melodicahaspa@gmail.com>
2023-06-01 03:55:51 +0200

    Gradle Java 19 workaround no longer neccessary due to new pom files

--- build_with_gradle.sh
+++ build_with_gradle.sh
@@ -4,6 +4,4 @@ set -e
 
 cd "$1"
 gradle init </dev/null
-# For some reason, Gradle isn't able to determine the source compability correctly. This is an ugly workaround for that.
-sed -i 's/java.sourceCompatibility = JavaVersion.*/java.sourceCompatibility = JavaVersion.VERSION_19/' build.gradle
 gradle publishToMavenLocal
